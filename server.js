1c4e64ad53272b55d340d973cf854b4e1762ee9c
车晓东 <187064449@qq.com>
2020-04-08 23:41:18 +0800

    task 更新了router功能🚇

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const Koa = require('koa');
 const next = require('next');
+const Router = require('koa-router');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
+const server = new Koa();
+const router = new Router();
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-    const server = new Koa()
+router.get('/test/:id', (ctx) => {
+    ctx.body = `your id ${ctx.params.id}`
+})
+
+server.use(router.routes());
+
+server.listen(3000, () => {
+    console.log('3000 🦁');
 });
