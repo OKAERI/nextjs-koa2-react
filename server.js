94d7812f2bbefaa556514c64474cbe06308a8f75
车晓东 <187064449@qq.com>
2020-04-08 22:30:42 +0800

    task ✨首次提交✨

---
+++ server.js
@@ -0,0 +1,10 @@
+const Koa = require('koa');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    const server = new Koa()
+});
